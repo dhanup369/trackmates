c7b03c2e55db2d59f2f7a9cf491e26ced1caf330
jitendrakumawat <kumawat.jitendra02@gmail.com>
2016-12-06 12:26:45 +0530

    add templates

--- trackmates/settings.py
+++ trackmates/settings.py
@@ -74,16 +74,13 @@ WSGI_APPLICATION = 'trackmates.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'trackmates$trackmatesdb',
-        'USER': 'trackmates',
-        'PASSWORD': 'india@mysql',
-        'HOST': 'bharatsush.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
